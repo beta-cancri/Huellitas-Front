3e155932839143a565fdb562bdcd97917740e9e1
Diego Chinchay <149448905+beta-cancri@users.noreply.github.com>
2024-08-22 09:40:41 -0500

    first changes

---
+++ Salvando Huellitas/src/App.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch, useLocation } from 'react-router-dom';
+import { Provider, useDispatch } from 'react-redux';
+import store from './redux/store';
+import HomePage from './views/home/home.component';
+import DetailPage from './views/detail/detail.component';
+import Navbar from './components/navbar/navbar.component';
+import './App.css';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <AppContent />
+      </Router>
+    </Provider>
+  );
+};
+
+const AppContent = () => {
+  const location = useLocation();
+  const [search, setSearch] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSearchSubmit = async (e) => {
+    e.preventDefault();
+    if (search) {
+      console.log(`Dispatching search for: ${search}`);
+      await dispatch(fetchPokemonByName(search));
+    } else {
+      await dispatch(fetchPokemons());
+    }
+  };
+
+  const handleClearSearch = async () => {
+    setSearch('');
+    await dispatch(fetchPokemons());
+  };
+
+  return (
+    <>
+      {location.pathname !== '/' && (
+        <Navbar 
+          search={search} 
+          setSearch={setSearch} 
+          handleSearchSubmit={handleSearchSubmit} 
+          handleClearSearch={handleClearSearch}
+        />
+      )}
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/home">
+          <HomePage search={search} setSearch={setSearch} handleSearchSubmit={handleSearchSubmit} />
+        </Route>
+        <Route path="/detail/:id" component={DetailPage} />
+        <Route path="/create" component={CreatePage} />
+      </Switch>
+    </>
+  );
+};
+
+export default App;
